fc2507b63bcf54f7e6c8530072783c6354ff7df8
Decan29 <58521030+Decan29@users.noreply.github.com>
2024-02-16 09:46:24 +0100

    Added second page for settings

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
-
-class MainGridLayout(GridLayout):
-    pass
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class MainApp(App):
     def build(self):
-        return Builder.load_file("main.kv")
+        return Builder.load_file("windows.kv")
+    
+class MainWindow(Screen):
+    pass
+class SettingsWindow(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
     
 if __name__ == "__main__":
     MainApp().run()
